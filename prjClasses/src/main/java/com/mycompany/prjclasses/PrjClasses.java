594cad13e287887645cad9332441c423f8c3660b
SimamkeleMsizi <ST10441818@vcconnect.edu.za>
2025-03-04 11:38:48 +0200

    updated code

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,6 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! When is 12pm mannnnn??!");
     }
 }
